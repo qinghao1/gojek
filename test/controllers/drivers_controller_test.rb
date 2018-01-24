7bba48a71d4c322b392eafa43a55c0a6aa1c35df
Chu Qing Hao <633703+qinghao1@users.noreply.github.com>
2018-01-24 23:16:38 +0800

    add simple customer tests

--- test/controllers/drivers_controller_test.rb
+++ test/controllers/drivers_controller_test.rb
@@ -1,7 +1,55 @@
 require 'test_helper'
 
 class DriversControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
+	test "Missing customer parameters" do
+		get :index
+		assert_response 422
+	end
+
+	test "Correct customer parameters" do
+		get :index, {
+			latitude: 1,
+			longitude: 1,
+		}
+		assert_response :success
+		expected_response = [
+			{
+				id: 1,
+				lattitude: 1,
+				longitude: 1,
+				distance: 0,
+			},
+			{
+				id: 2,
+				lattitude: 1,
+				longitude: 2,
+				distance: 1,
+			}
+		].to_json
+		assert_equal @response.body, expected_response
+	end
+
+	test "Correct customer parameters but too far" do
+		get :index, {
+			latitude: 10,
+			longitude: 10,
+			radius: 1,
+		}
+		assert_response :success
+		assert_empty @response.body
+	end
+
+	test "Wrong customer parameters" do
+		get :index, {
+			latitude: 100,
+			longitude: 100,
+			radius: 1,
+		}
+		assert_response 422
+		expected_response = {
+			errors: [
+				"Latitude should be between +/- 90",
+			]
+		}
+		assert_equal @response.body, expected_response
+	end
