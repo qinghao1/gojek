6bcd5ccb5da1e158dcf8737002aebfd73e0d3987
Chu Qing Hao <633703+qinghao1@users.noreply.github.com>
2018-01-25 21:15:40 +0800

    improve model validations

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,3 +1,11 @@
 class Driver < ApplicationRecord
-  validates_presence_of :id, :lonlat
+  validates_presence_of :id, :lonlat, :accuracy
+  validates :id, numercality: {
+    greater_than_or_equal_to: 1,
+    less_than_or_equal_to: 50000,
+  }
+  validates :accuracy, numercality: {
+    greater_than_or_equal_to: 0,
+    less_than_or_equal_to: 1,
+  }
 end
