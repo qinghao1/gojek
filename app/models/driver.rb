84d9ed4d2971365a45d89ba13df3ed610f260877
Chu Qing Hao <633703+qinghao1@users.noreply.github.com>
2018-01-25 21:16:13 +0800

    typo

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,10 +1,10 @@
 class Driver < ApplicationRecord
   validates_presence_of :id, :lonlat, :accuracy
-  validates :id, numercality: {
+  validates :id, numericality: {
     greater_than_or_equal_to: 1,
     less_than_or_equal_to: 50000,
   }
-  validates :accuracy, numercality: {
+  validates :accuracy, numericality: {
     greater_than_or_equal_to: 0,
     less_than_or_equal_to: 1,
   }
