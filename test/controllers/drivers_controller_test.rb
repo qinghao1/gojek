48e864d434d1f7ea069426661270e212051a5553
Chu Qing Hao <633703+qinghao1@users.noreply.github.com>
2018-01-25 23:38:45 +0800

    fleshed out customer api tests

--- test/controllers/drivers_controller_test.rb
+++ test/controllers/drivers_controller_test.rb
@@ -1,50 +1,53 @@
 require 'test_helper'
 
 class DriversControllerTest < ActionDispatch::IntegrationTest
+  fixtures :drivers
+
 	test "Missing customer parameters" do
-		get :index
+		get "/drivers"
 		assert_response 422
 	end
 
 	test "Correct customer parameters" do
-		get :index, {
-			latitude: 1,
-			longitude: 1,
-		}
+		get "/drivers", {
+      params: {
+  			latitude: 1,
+  			longitude: 1,
+  		}
+    }
 		assert_response :success
 		expected_response = [
 			{
 				id: 1,
-				latitude: 1,
-				longitude: 1,
-				distance: 0,
-			},
-			{
-				id: 2,
-				latitude: 1,
-				longitude: 2,
-				distance: 1,
+        distance: 0.0,
+        longitude: 1.0,
+				latitude: 1.0,
 			}
 		].to_json
 		assert_equal @response.body, expected_response
 	end
 
 	test "Correct customer parameters but too far" do
-		get :index, {
-			latitude: 10,
-			longitude: 10,
-			radius: 1,
-		}
+		get "/drivers", {
+      params: {
+  			latitude: 10,
+  			longitude: 10,
+  			radius: 1,
+  		}
+    }
+    expected_response = [].to_json
 		assert_response :success
-		assert_empty @response.body
+		assert_equal @response.body, expected_response
 	end
 
-	test "Wrong customer parameters" do
-		get :index, {
-			latitude: 100,
-			longitude: 100,
-			radius: 1,
-		}
+	test "Wrong customer parameters (Wrong lat)" do
+		get "/drivers", {
+      params: {
+  			latitude: 100,
+  			longitude: 10,
+  			radius: 1,
+  		}
+    }
 		assert_response 422
 		expected_response = {
 			errors: [
@@ -54,30 +57,101 @@ class DriversControllerTest < ActionDispatch::IntegrationTest
 		assert_equal @response.body, expected_response
 	end
 
+    test "Wrong customer parameters (Wrong long)" do
+    get "/drivers", {
+      params: {
+        latitude: 10,
+        longitude: 100,
+        radius: 1,
+      }
+    }
+    assert_response 422
+    expected_response = {
+      errors: [
+        "Longitude should be between +/- 90",
+      ]
+    }.to_json
+    assert_equal @response.body, expected_response
+  end
+
+    test "Wrong customer parameters (Wrong lat/long)" do
+    get "/drivers", {
+      params: {
+        latitude: 100,
+        longitude: 100,
+        radius: 1,
+      }
+    }
+    assert_response 422
+    expected_response = {
+      errors: [
+        "Latitude should be between +/- 90",
+        "Longitude should be between +/- 90",
+      ]
+    }.to_json
+    assert_equal @response.body, expected_response
+  end
+
+    test "Wrong customer parameters (Non-numeric)" do
+    get "/drivers", {
+      params: {
+        latitude: "aaaa",
+        longitude: 10,
+        radius: 1,
+      }
+    }
+    assert_response 422
+    expected_response = {
+      errors: [
+        "Latitude should be between +/- 90",
+      ]
+    }.to_json
+    assert_equal @response.body, expected_response
+  end
+
 	test "Correct driver parameters" do
-		get :location, {
+		put :location, {
 			id: 1,
-			latitude: 1,
-			longitude: 2,
-			accuracy: 0.7
+      params: {
+        latitude: 1,
+        longitude: 2,
+        accuracy: 0.7,
+      }
 		}
 		assert_response :success
 		assert_empty @response.body
 	end
 
 	test "Decimal driver parameters" do
-		get :location, {
+		put :location, {
 			id: 2,
-			latitude: 1.12211212,
-			longitude: 2.22333334,
-			accuracy: 0.7
+      params: {
+        latitude: 1.2222,
+        longitude: 2,
+        accuracy: 0.7,
+      }
 		}
 		assert_response :success
 		assert_empty @response.body
 	end
 
+  test "Non-numeric driver parameters" do
+    put :location, {
+      id: 2,
+      latitude: "string",
+      longitude: 2.22333334,
+    }
+    assert_response 422
+    expected_response = {
+      errors: [
+        "Latitude should be between +/- 90",
+      ]
+    }.to_json
+    assert_equal @response.body, expected_response
+  end
+
 	test "Invalid driver id" do
-		get :location, {
+		put :location, {
 			id: 99999,
 			latitude: 1,
 			longitude: 2,
@@ -88,8 +162,8 @@ class DriversControllerTest < ActionDispatch::IntegrationTest
 	end
 
 	test "Wrong driver parameters" do
-		get :location, {
-			id: 1
+		put :location, {
+			id: 1,
 			latitude: 100,
 			longitude: 100,
 			accuracy: 0.7
