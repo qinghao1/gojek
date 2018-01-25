9eebd2ca5d08f4b744a59dfc3ae7d62d5051ba32
Chu Qing Hao <633703+qinghao1@users.noreply.github.com>
2018-01-25 20:28:47 +0800

    finish preliminary customer SQL query

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,35 +1,40 @@
 class DriversController < ApplicationController
   def index
-    # Check for errors:
-    errors = []
-
     begin
+      # Check for errors:
+      errors = []
       sanitized = sanitize_and_normalize_params(params)
-    rescue ActionController::ParameterMissing
-      errors.push "Missing latitude and/or longitude"
-    end
-    # Check latitude
-    if !valid_coord sanitized[:longitude]
-      errors.push "Latitude should be between +/- 90"
-    end
-    # Check longitude
-    if !valid_coord sanitized[:longitude]
-      errors.push "Longitude should be between +/- 90"
-    end
 
-    # Return 422 response if there are errors
-    if errors
-      render {
-        json: {
-          errors: errors,
-        }
-        status: :unprocessable_entity,
-      }
-    # Else, render successful response
-    else
-      render {
-        json: closest_drivers(sanitized)
-      }
+      # Check latitude
+      if !valid_coord(sanitized[:longitude])
+        errors.push "Latitude should be between +/- 90"
+      end
+      # Check longitude
+      if !valid_coord(sanitized[:longitude])
+        errors.push "Longitude should be between +/- 90"
+      end
+
+      # Return 422 response if there are errors
+      if !errors.empty?
+        render({
+          json: {
+            errors: errors,
+          },
+          status: :unprocessable_entity,
+        })
+      # Else, render successful response
+      else
+        render({
+          json: closest_drivers(sanitized).to_json,
+        })
+      end
+    rescue ActionController::ParameterMissing
+        render({
+          json: {
+            errors: ["Missing latitude and/or longitude"],
+          },
+          status: :unprocessable_entity,
+        })
     end
   end
 
@@ -37,21 +42,23 @@ class DriversController < ApplicationController
   private
   def sanitize_and_normalize_params(params)
     params
-    .require(
-      :latitude,
-      :longitude,
-    )
     .permit(
       :latitude,
       :longitude,
       :radius,
       :limit,
     )
+    .require(
+      [
+        :latitude,
+        :longitude,
+      ]
+    )
 
     params[:latitude] = params[:latitude].to_f
     params[:longitude] = params[:longitude].to_f
-    params[:radius] = params[:radius].to_f or 500
-    params[:limit] = params[:limit].to_i or 10
+    params[:radius] = params[:radius] ? params[:radius].to_f : 500
+    params[:limit] = params[:limit] ? params[:limit].to_f : 10
 
     return params
   end
@@ -61,17 +68,29 @@ class DriversController < ApplicationController
     # Important: Parameters must be sanitized because
     # can't use in-built ActiveRecord protections with PostGIS
 
-    Driver
-    .where("ST_Distance(
-        latlon, 
-        'POINT(#{sanitized_params[:latitude]}, #{sanitized_params[:longitude]})'
-      ) < #{sanitized_params[:radius]}"
-    )
-    .limit(sanitized_params[:limit])
+    distance_query = 
+      Driver
+      .select(
+        "ST_Distance(
+          lonlat, 
+          'POINT(#{sanitized_params[:longitude]} #{sanitized_params[:latitude]})'
+        ) AS distance",
+        "id",
+        "ST_X(lonlat::geometry) AS longitude",
+        "ST_Y(lonlat::geometry) AS latitude"
+      )
+      .to_sql
 
+    restricted_distance_query = 
+    "EXPLAIN SELECT * FROM (#{distance_query}) dq "\
+    "WHERE dq.distance < #{sanitized_params[:radius]} "\
+    "ORDER BY dq.distance "\
+    "LIMIT #{sanitized_params[:limit]} "
+
+    return ActiveRecord::Base.connection.execute(restricted_distance_query)
   end
 
-  def valid_coord(latlon)
-    return latlon.is_a?latlon >= -90 and latlon <= 90
+  def valid_coord(lonlat)
+    return (lonlat >= -90 and lonlat <= 90)
   end
 end
