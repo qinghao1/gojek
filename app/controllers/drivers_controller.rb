6a3a0c05b623ea053ed70e8955100011fb156041
Chu Qing Hao <633703+qinghao1@users.noreply.github.com>
2018-01-26 06:15:59 +0800

    improve code style

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,16 +1,16 @@
 class DriversController < ApplicationController
+  # GET /drivers
   def index
     begin
-      # Check for errors:
       errors = []
       sanitized = sanitize_and_normalize_customer_params(params)
 
       # Check latitude
-      if !valid_coord(sanitized[:latitude])
+      if !valid_coord?(sanitized[:latitude])
         errors.push "Latitude should be between +/- 90"
       end
       # Check longitude
-      if !valid_coord(sanitized[:longitude])
+      if !valid_coord?(sanitized[:longitude])
         errors.push "Longitude should be between +/- 90"
       end
 
@@ -22,12 +22,13 @@ class DriversController < ApplicationController
           },
           status: :unprocessable_entity,
         })
-      # Else, render successful response
+      # No errors, render successful response
       else
         render({
           json: closest_drivers(sanitized).to_json,
         })
       end
+
     # Missing latitude/longitude params
     rescue ActionController::ParameterMissing
         render({
@@ -39,14 +40,14 @@ class DriversController < ApplicationController
     end
   end
 
+  # PUT /drivers/:id/location
   def location
     begin
-      # Check for errors:
       errors = []
       sanitized = sanitize_and_normalize_driver_req(request.raw_post)
 
       # Check id
-      if !valid_id(params[:id].to_i)
+      if !valid_id?(params[:id].to_i)
         render(
           json: {},
           status: :not_found,
@@ -54,15 +55,15 @@ class DriversController < ApplicationController
         return
       end
       # Check latitude
-      if !valid_coord(sanitized["latitude"])
+      if !valid_coord?(sanitized["latitude"])
         errors.push "Latitude should be between +/- 90"
       end
       # Check longitude
-      if !valid_coord(sanitized["longitude"])
+      if !valid_coord?(sanitized["longitude"])
         errors.push "Longitude should be between +/- 90"
       end
       # Check accuracy
-      if !valid_accuracy(sanitized["accuracy"])
+      if !valid_accuracy?(sanitized["accuracy"])
         errors.push "Accuracy should be between 0 and 1"
       end
 
@@ -74,23 +75,25 @@ class DriversController < ApplicationController
           },
           status: :unprocessable_entity,
         })
-      # Else, render successful response
+      # No errors, continue
       else
+        # Create/update driver record
         driver = Driver.find_by_id(params[:id])
         if driver
           driver.lonlat = 
-          "POINT(#{sanitized["longitude"]}"\
-          " #{sanitized["latitude"]})"
+            "POINT(#{sanitized["longitude"]}"\
+            " #{sanitized["latitude"]})"
           driver.accuracy = sanitized["accuracy"]
         else
           driver = Driver.new({
-          id: params[:id],
-          lonlat: "POINT(#{sanitized["longitude"]}"\
-                  " #{sanitized["latitude"]})",
-          accuracy: sanitized["accuracy"],
-        })
+            id: params[:id],
+            lonlat: "POINT(#{sanitized["longitude"]}"\
+                    " #{sanitized["latitude"]})",
+            accuracy: sanitized["accuracy"],
+          })
         end
 
+        # Save driver record and render either success or 422
         begin
           driver.save!
           render({
@@ -106,6 +109,8 @@ class DriversController < ApplicationController
           return
         end
       end
+
+    # Missing latitude/longitude params
     rescue ActionController::ParameterMissing
         render({
           json: {
@@ -119,6 +124,8 @@ class DriversController < ApplicationController
 
   # Helper methods
   private
+
+  # Given params hash, sanitizes it and sets defaults
   def sanitize_and_normalize_customer_params(params)
     params
     .permit(
@@ -135,30 +142,30 @@ class DriversController < ApplicationController
     )
 
     # Check that latitude is a decimal number
-    if /\d+(\.\d+)?/ =~ params[:latitude]
+    if DECIMAL_REGEX =~ params[:latitude]
       params[:latitude] = params[:latitude].to_f
     else
       params[:latitude] = nil
     end
 
     # Check that longitude is a decimal number
-    if /\d+(\.\d+)?/ =~ params[:longitude]
+    if DECIMAL_REGEX =~ params[:longitude]
       params[:longitude] = params[:longitude].to_f
     else
       params[:longitude] = nil
     end
 
-    # Check that radius is a decimal number.
-    # If it doesn't exist, set to 500 default
-    if params[:radius] and /\d+(\.\d+)?/ =~ params[:radius]
+    # Check that radius is a decimal number if it exists
+    # Else, set to 500 default
+    if params[:radius] and DECIMAL_REGEX =~ params[:radius]
       params[:radius] = params[:radius].to_f 
     else
       params[:radius] = 500
     end
 
-    # Check that limit is a decimal number.
-    # If it doesn't exist, set to 10 default
-    if params[:limit] and /\d+(\.\d+)?/ =~ params[:limit]
+    # Check that limit is a decimal number if it exists
+    # Else, set to 10 default
+    if params[:limit] and DECIMAL_REGEX =~ params[:limit]
       params[:limit] = params[:limit].to_f 
     else
       params[:limit] = 10
@@ -167,10 +174,11 @@ class DriversController < ApplicationController
     return params
   end
 
+  # Given req string, returns sanitized request as a hash
   def sanitize_and_normalize_driver_req(req)
     begin
       req = JSON.parse(req)
-    # If request is wonky, JSON parser will throw error
+    # Return dummy request if JSON is wonky
     rescue JSON::ParserError
       return {
         "latitude" => nil,
@@ -190,22 +198,22 @@ class DriversController < ApplicationController
     return req
   end
 
-  # Returns driver objects given query parameters in params
+  # Returns driver objects given query parameters
+  # Important: Parameters must be sanitized because
+  # of string interpolation. Unfortunately PostGIS demands this
   def closest_drivers(sanitized_params)
-    # Important: Parameters must be sanitized because
-    # can't use in-built ActiveRecord protections with PostGIS
-
+    # Query to find nearest $limit drivers
     nearest_k_drivers = Driver
     .select(
-      "ST_Distance(
-        lonlat, 
-        'POINT(#{sanitized_params[:longitude]} #{sanitized_params[:latitude]})'
-      ) AS distance",
+      "ST_Distance("\
+      " lonlat,"\
+      " 'POINT(#{sanitized_params[:longitude]} #{sanitized_params[:latitude]})'"\
+      ") AS distance",
       "id",
       "ST_X(lonlat::geometry) AS longitude",
       "ST_Y(lonlat::geometry) AS latitude"
     )
-    .order(
+    .order( # Order by spatial distance; this is optimized by PostGIS
       "lonlat"\
       " <-> "\
       "'POINT(#{sanitized_params[:longitude]} #{sanitized_params[:latitude]})'"
@@ -213,22 +221,26 @@ class DriversController < ApplicationController
     .limit(sanitized_params[:limit])
     .to_sql
 
+    # Modify query to filter to those within $radius
     nearest_k_drivers_within_radius = 
     "SELECT * FROM (#{nearest_k_drivers}) x "\
     "WHERE distance < #{sanitized_params[:radius]}"
 
+    # Execute query
     ActiveRecord::Base.connection.execute(nearest_k_drivers_within_radius)
   end
 
-  def valid_coord(lonlat)
+  def valid_coord?(lonlat)
     return (lonlat and (lonlat >= -90 and lonlat <= 90))
   end
 
-  def valid_id(id)
+  def valid_id?(id)
     return (id >= 1 and id <= 50000)
   end
 
-  def valid_accuracy(acc)
+  def valid_accuracy?(acc)
     return (acc >= 0 and acc <= 1)
   end
+
+  DECIMAL_REGEX = /\d+(\.\d+)?/.freeze
 end
