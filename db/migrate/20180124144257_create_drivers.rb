d805793491aa6749150bb77c1ad8b666c148f619
Chu Qing Hao <633703+qinghao1@users.noreply.github.com>
2018-01-25 19:18:07 +0800

    add spatial indexing to drivers table

--- db/migrate/20180124144257_create_drivers.rb
+++ db/migrate/20180124144257_create_drivers.rb
@@ -1,7 +1,12 @@
 class CreateDrivers < ActiveRecord::Migration[5.1]
   def change
+    # Create Drivers table
     create_table :drivers do |t|
       t.st_point :lonlat, geographic: true
+    end
+    # Use spatial indexing
+    change_table :drivers do |t|
+      t.index :lonlat, using: :gist
     end
   end
 end
