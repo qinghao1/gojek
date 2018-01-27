8c35ed65ceff942720dd45658e7cac1647b12ee2
Chu Qing Hao <633703+qinghao1@users.noreply.github.com>
2018-01-27 14:48:59 +0800

    travis

--- travis-install-pg10.sh
+++ travis-install-pg10.sh
@@ -9,4 +9,3 @@ sudo cp /etc/postgresql/{9.6,10}/main/pg_hba.conf
 
 echo "Restarting Postgres 10"
 sudo service postgresql restart
-sudo psql -c 'CREATE ROLE travis SUPERUSER LOGIN CREATEDB;' -U postgres
