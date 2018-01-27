9e2023b71585f145846c824bd66f87cc4690d76b
Chu Qing Hao <633703+qinghao1@users.noreply.github.com>
2018-01-27 15:01:37 +0800

    travis

--- travis-install-pg10-postgis.sh
+++ travis-install-pg10-postgis.sh
@@ -5,6 +5,7 @@ set -ex
 echo "Installing Postgres 10"
 sudo service postgresql stop
 sudo apt-get install -q postgresql-10 postgresql-client-10
+sudo apt-get install -q -y postgresql-10-postgis-2.4-scripts postgresql-10-postgis-2.4 postgis
 sudo cp /etc/postgresql/{9.6,10}/main/pg_hba.conf
 
 echo "Restarting Postgres 10"
